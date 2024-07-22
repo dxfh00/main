df02aa015aee72d24da012c9d7e155f22b684cea
Anton <132255178+dxfh00@users.noreply.github.com>
2024-07-22 20:04:38 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st program
+print(5*9**0.5)
+
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+# 3rt program
+print((1234//10%100)+(5678//10%100))
+
+# th program4
+print(int(42.13)==(13.42*100%100)) and (int(13.42)==(42.13*100%100))
