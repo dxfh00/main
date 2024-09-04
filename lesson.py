cc7e0090802a88d5359eba7ddd74470681abe493
Anton <132255178+dxfh00@users.noreply.github.com>
2024-09-04 17:45:18 +0300

    lesson.py

--- lesson.py
+++ lesson.py
@@ -1,18 +1,63 @@
-def calculate_structure_sum(data_structure):
-    result = 0
-    if isinstance(data_structure, int):
-        result += data_structure
-    elif isinstance(data_structure, (list, tuple, set)):
-        for i in data_structure:
-            # result += calculate_structure_sum(i)
-            calculate_structure_sum(i)
-    elif isinstance(data_structure, str):
-        result += len(data_structure)
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
 
-    return result
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        min_floors = 0
+        if new_floor <= min_floors or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+        else:
+            while min_floors < new_floor:
+                min_floors += 1
+                print(min_floors)
+
+    def __str__(self):
+        return (f"Название: {self.name}: {self.number_of_floors}")
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        return self.number_of_floors + value
 
 
-data_structure = ['55555', [1, 3, 2, 4], '666666']
+h1 = House('ЖК Горский', 10)
+h2 = House('Домик в деревне', 20)
+# h1.go_to(5)
+# h2.go_to(10)
 
+print(h1)
+print(h2)
+# print(len(h1))
+# print(len(h2))
 
-print(calculate_structure_sum(data_structure))
+print(h1 == h2)
+h1 = h1 + 10
+print(h1)
+# print(h1 == h2)
+
+# print(h1 > h2)
+# print(h1 >= h2)
+# print(h1 < h2)
+# print(h1 <= h2)
+# print(h1 != h2)
