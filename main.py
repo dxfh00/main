5696e021ae3d7a23151083729be34a1f93fa8724
Anton <132255178+dxfh00@users.noreply.github.com>
2024-09-22 20:02:48 +0300

    main.py

--- main.py
+++ main.py
@@ -1,11 +1,39 @@
-# 1st program
-print(5*9**0.5)
+from pprint import pprint
 
-# 2nd program
-print(9.99>9.98 and 1000!=1000.1)
 
-# 3rt program
-print((1234//10%100)+(5678//10%100))
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
 
-# th program4
-print((int(42.13)==(13.42*100%100)) and (int(13.42)==(42.13*100%100)))
+    def __str__(self):
+        return (self.name, self.weight, self.category)
+
+
+class Shop(Product):
+    __file_name = 'products.txt'
+    def __init__(self):
+        Product.__init__(self)
+
+    def get_products(self, file):
+        self.file = file
+        self.file = open(__file_name, 'r')
+        self.file.close()
+        return self.file
+
+    def add(self, *products):
+        self.products = products
+        self.file = open(__file_name, 'w')
+
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)  # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
